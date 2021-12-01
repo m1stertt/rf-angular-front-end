d2a99ceb5c26940323032ec47152dc494e02058e
m1stertt <52206366+m1stertt@users.noreply.github.com>
2021-12-01 18:16:17 +0100

    View Products.

--- src/app/products/product-create/product-create.component.ts
+++ src/app/products/product-create/product-create.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductDto} from "../shared/product.dto";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProductsService} from "../shared/products.service";
+import {Location} from "@angular/common";
 
 @Component({
-  selector: 'app-product-create',
+  selector: 'app-inno-tech-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.scss']
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  product: ProductDto;
+
+  constructor(private route: ActivatedRoute,
+              private productsService: ProductsService,
+              private location: Location,
+              private router: Router) {
+    this.product = {id: 0, name:'', price: 0, description: '', imageUrl: ''}
+  }
 
   ngOnInit(): void {
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  create() {
+    this.productsService.create(this.product).subscribe(() => {
+        this.router.navigateByUrl('/products')
+      },
+      error => {
+        console.log(error)
+      });
+
+  }
 }
