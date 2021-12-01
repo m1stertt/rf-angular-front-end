768de4906aa8b3e23d4de154141b3905c3fbf3aa
m1stertt <52206366+m1stertt@users.noreply.github.com>
2021-12-01 17:26:33 +0100

    Initial

---
+++ src/app/products/products-list/products-list.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {ProductsService} from '../shared/products.service';
+import {ProductDto} from '../shared/product.dto';
+import {Router} from "@angular/router";
+import { AppComponent } from 'src/app/app.component';
+
+@Component({
+  selector: 'app-inno-tech-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.scss']
+})
+
+export class ProductsListComponent implements OnInit {
+  products: ProductDto[]=[];
+  clickedProduct:ProductDto | undefined;
+  showWriteProducts:Boolean | undefined;
+
+  constructor(private _productService: ProductsService,
+              private router: Router,private appComponent:AppComponent) {
+  }
+
+  ngOnInit(): void {
+    this.updateList()
+  }
+
+  // delete(product: ProductDto) {
+  //   this._productService.delete(product.id).subscribe(() => this.updateList());
+  // }
+
+  updateList(): void {
+    this._productService.getAll()
+      // Not until this is called the request is sent
+      .subscribe(products => {
+        this.products = products;
+      });
+    this.appComponent.profile$?.subscribe(pro=>{
+      console.log(pro?.permissions);
+      this.showWriteProducts=pro?.permissions.includes("CanWriteProducts");
+    });
+  }
+
+  create() {
+    this.router.navigateByUrl('products/create');
+  }
+}
