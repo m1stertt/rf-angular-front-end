14a5e4be28ae1adea65a25f65133cf107efb2630
Thomas Truelsen <52206366+m1stertt@users.noreply.github.com>
2021-12-02 18:35:50 +0100

    ts

--- src/app/products/producsts-grid/producsts-grid.component.ts
+++ src/app/products/producsts-grid/producsts-grid.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductsService} from "../shared/products.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import {ProductDto} from "../shared/product.dto";
+import {Location} from "@angular/common";
+
 
 @Component({
-  selector: 'app-producsts-grid',
-  templateUrl: './producsts-grid.component.html',
-  styleUrls: ['./producsts-grid.component.scss']
+  selector: 'app-products-grid',
+  templateUrl: './products-grid.component.html',
+  styleUrls: ['./products-grid.component.scss']
 })
-export class ProducstsGridComponent implements OnInit {
+export class ProductsGridComponent implements OnInit {
+  products: ProductDto[] = [];
+  breakpoint: number | undefined;
 
-  constructor() { }
+  lowValue: number = 0;
+  highValue: number = 20;
+
+  constructor(private route: ActivatedRoute,
+              private productsService: ProductsService,
+              private location: Location,
+              private router: Router){}
 
   ngOnInit(): void {
+    this.breakpoint = (window.innerWidth <= 400) ? 1 : 2;
+    this.updateList()
+
   }
+  updateList(): void {
+    this.productsService.getAll()
+      // Not until this is called the request is sent
+      .subscribe(products => {
+        this.products = products;
+      });
+  }
+
+  public getPaginatorData(event: PageEvent): PageEvent {
+    this.lowValue = event.pageIndex * event.pageSize;
+    this.highValue = this.lowValue + event.pageSize;
+    return event;
+  }
+
+  onResize(event: any) {
+    this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 2;
+  }
+
 
 }
