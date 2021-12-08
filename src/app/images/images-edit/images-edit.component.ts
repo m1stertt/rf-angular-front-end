5ec592d96153d2a2ee11466b678a4cd61e19f67e
Snoozy <snoozy1995@gmail.com>
2021-12-09 00:19:41 +0100

    Images feature

---
+++ src/app/images/images-edit/images-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-images-edit',
+  templateUrl: './images-edit.component.html',
+  styleUrls: ['./images-edit.component.scss']
+})
+export class ImagesEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
