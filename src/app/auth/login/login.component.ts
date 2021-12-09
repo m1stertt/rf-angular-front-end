087fbed89c9a4bcdaa323390fd2116ce211f3c17
Thomas Truelsen <52206366+m1stertt@users.noreply.github.com>
2021-12-09 23:16:16 +0100

    Small fixes for user-registration.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -18,16 +18,20 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
   loginForm = new FormGroup({
-    username: new FormControl('',
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+    ]),
+
+    password: new FormControl('',
       [
         Validators.required,
-        Validators.minLength(3)
+        Validators.minLength(6)
       ]
     ),
-    password: new FormControl('', Validators.required),
   });
 
-  get username() {return this.loginForm.get('username');}
+  get email() {return this.loginForm.get('email');}
   get password() {return this.loginForm.get('password');}
 
   login() {
