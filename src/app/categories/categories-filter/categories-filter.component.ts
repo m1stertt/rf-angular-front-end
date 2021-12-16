aa036344dd59b90ad88376748f1d1eb6be960a82
m1stertt <52206366+m1stertt@users.noreply.github.com>
2021-12-16 17:06:05 +0100

    Semi works.

--- src/app/categories/categories-filter/categories-filter.component.ts
+++ src/app/categories/categories-filter/categories-filter.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, EventEmitter, OnInit, Output} from '@angular/c
 import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {ColorDto} from "../../colors/shared/color.dto";
 import {ProductDto} from "../../products/shared/product.dto";
+import {ColorsService} from "../../colors/shared/colors.service";
 
 @Component({
   selector: 'app-categories-filter',
@@ -10,38 +11,20 @@ import {ProductDto} from "../../products/shared/product.dto";
 })
 export class CategoriesFilterComponent implements OnInit, AfterViewInit {
   @Output() loaded = new EventEmitter();
-  // colorFormGroup: FormGroup;
+  @Output() color = new EventEmitter<string>();
 
-  // colorsData: ColorDto[] = [
-  //   {id: 1, title: 'red', colorString: 'red', disabled: false, selected: false, products: []},
-  //   {id: 2, title: 'blue', colorString: 'blue', disabled: false, selected: false, products: []},
-  //   {id: 3, title: 'red', colorString: 'red', disabled: false, selected: false, products: []},
-  //   {id: 4, title: 'red', colorString: 'red', disabled: false, selected: false, products: []},
-  // ];
+  colorsData?: ColorDto[];
+  colors: FormGroup;
 
-
-  // constructor(fb: FormBuilder) {
-  //   this.colorFormGroup = fb.group({
-  //     colors: []
-  //   });
-  // }
-  toppings: FormGroup;
-
-  constructor(fb: FormBuilder) {
-    this.toppings = fb.group({
-      pepperoni: false,
-      extracheese: false,
-      mushroom: false,
-    });
+  constructor(private colorService: ColorsService, fb: FormBuilder) {
+    this.colors = fb.group({})
   }
-
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
     this.loaded.emit();
-  }
+    this.colorService.getAll().subscribe(value => this.colorsData = value);  }
 
 }
